db0180c1621e35d1f73c104687631b17e4ce7448
Mikhail Saveliev <Savelievmv@gmail.com>
2020-11-06 23:47:38 +0100

    Input handling in progress

--- task1.c
+++ task1.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+// Read array
+int* readintarray(int* nr)
+{
+	int* arr;
+	int arrsize;
+	int i;
+	
+	scanf("%d", &arrsize);
+
+	arr = malloc(sizeof(int) * arrsize);
+	if (arr == NULL) {
+		perror("Malloc failed");
+		exit(1);
+	}
+	for (i = 0; i < arrsize; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	*nr = arrsize;
+	
+	return arr;
+}
 
 int main(void)
 {
@@ -7,14 +32,44 @@ int main(void)
 	int start; // Initial planet
 	int terminal; // Terminal planet
 	int infectstops; // Stops after crew was infected
+	int numofinfectedplanets; // Amount of infected planets 
+	int* infplanetid; // Array of inf planets ids
+	int i;
+	int planetswithvaccine; // Amount of planets with vaccine
+	int* planetswithvaccineids; // Array of planets with vaccine ids
 
-        // Reads number of planes and number of paths
-	scanf("%d %d %d %d %d", &nplanets, &mpaths);
-	printf("num of planets = %d, num of paths = %d\n", nplanets, mpaths);
+    // Reads number of planes and number of paths
+	scanf("%d %d", &nplanets, &mpaths);
 
-        // Reads number of initial planet, terminal planet and 
-	scanf("%d %d %d", &start, &terminal, &infectstops); 
-        printf()
+    // Reads number of initial planet, terminal planet 
+	// and stops after getting infected
+	scanf("%d %d %d", &start, &terminal, &infectstops);
+	
+	// Read amount of infected planets and their ids
+	infplanetid = readintarray(&numofinfectedplanets);
 
+	// Read amount of planets with vaccine and their ids
+	planetswithvaccineids = readintarray(&planetswithvaccine);
+
+	printf("num of planets = %d, num of paths = %d, init planet = %d, " 
+		   "temr planet = %d, infected stops = %d\n", nplanets, mpaths, 
+		    start, terminal, infectstops);
+
+	printf("infected planets: \n");
+	for (i = 0; i < numofinfectedplanets; i++) {
+		printf("%d ", infplanetid[i]);
+	}
+	printf("\n");
+
+	// Print planets with vaccines
+	printf("Planets with vaccine: \n");
+	for (i = 0; i < planetswithvaccine; i++) {
+		printf("%d ", planetswithvaccineids[i]);
+	}
+	printf("\n");
+
+	free(infplanetid);
+	free(planetswithvaccineids);
+	
 	return 0;
 }
