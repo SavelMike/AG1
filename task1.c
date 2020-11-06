a0f158027c475b4bc3b0b5c9a7952fe2db28831f
Saveliev Mikhail <savelievmv@gmail.com>
2020-11-06 22:11:21 +0100

    Moving to MSVS

--- task1.c
+++ task1.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main(void)
+{
+	int nplanets; // Num of planets
+	int mpaths; // Num of paths
+	int start; // Initial planet
+	int terminal; // Terminal planet
+	int infectstops; // Stops after crew was infected
+
+        // Reads number of planes and number of paths
+	scanf("%d %d %d %d %d", &nplanets, &mpaths);
+	printf("num of planets = %d, num of paths = %d\n", nplanets, mpaths);
+
+        // Reads number of initial planet, terminal planet and 
+	scanf("%d %d %d", &start, &terminal, &infectstops); 
+        printf()
+
+	return 0;
+}
