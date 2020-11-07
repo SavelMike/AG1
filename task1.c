5faa5a6b528a9a7c044c991082b1932e021001f4
Mikhail Saveliev <Savelievmv@gmail.com>
2020-11-07 18:45:04 +0100

    Graph representation as adjacancy list added

--- task1.c
+++ task1.c
@@ -25,6 +25,57 @@ int* readintarray(int* nr)
 	return arr;
 }
 
+struct intlist 
+{
+	int value;
+	struct intlist* next;
+};
+
+struct graphvertex
+{
+	int id; // identifier
+	int status; // infected or has vaccine
+	struct intlist* adjactedverteces; // neighboor verteces
+};
+
+// Insert new element to beginning of intlist
+// Arguments:
+//		value (data to be added to the list)
+//		head (pointer to list head, updated to new first element of list)
+// return nothing
+void add_intlist_first(int value, struct intlist** head)
+{
+	struct intlist* newelement;
+	newelement = malloc(sizeof(struct intlist));
+	if (newelement == NULL) {
+		perror("Malloc failed");
+		exit(1);
+	}
+	newelement->value = value;
+	newelement->next = *head;
+	*head = newelement;
+}
+
+void print_intlist(struct intlist* head)
+{
+	while (head != NULL) {
+		printf("%d ", head->value);
+		head = head->next;
+	}
+}
+
+void print_graph(struct graphvertex* graph, int nvertex)
+{
+	int i;
+	
+	for (i = 0; i < nvertex; i++) {
+		printf("id = %d status = %d adjacent vertices: ", graph[i].id, 
+				graph[i].status);
+		print_intlist(graph[i].adjactedverteces);
+		printf("\n");
+	}
+}
+
 int main(void)
 {
 	int nplanets; // Num of planets
@@ -37,6 +88,7 @@ int main(void)
 	int i;
 	int planetswithvaccine; // Amount of planets with vaccine
 	int* planetswithvaccineids; // Array of planets with vaccine ids
+	struct graphvertex* graph; // Array of verteces or the graph itself
 
     // Reads number of planes and number of paths
 	scanf("%d %d", &nplanets, &mpaths);
@@ -51,22 +103,34 @@ int main(void)
 	// Read amount of planets with vaccine and their ids
 	planetswithvaccineids = readintarray(&planetswithvaccine);
 
+	// Constuct a graph in adjustency vertices form
+	graph = malloc(sizeof(struct graphvertex) * nplanets);
+	if (graph == NULL) {
+		perror("Malloc failed");
+		exit(1);
+	}
+
+	// Initilize all fields of the array
+	for (i = 0; i < nplanets; i++) {
+		graph[i].id = i;
+		graph[i].status = 0;
+		graph[i].adjactedverteces = NULL;
+	}
+
+	// Read edges inf
+	for (i = 0; i < mpaths; i++) {
+		int v1;
+		int v2;
+		scanf("%d %d", &v1, &v2);
+		add_intlist_first(v1, &graph[v2].adjactedverteces);
+		add_intlist_first(v2, &graph[v1].adjactedverteces);
+	}
+
 	printf("num of planets = %d, num of paths = %d, init planet = %d, " 
-		   "temr planet = %d, infected stops = %d\n", nplanets, mpaths, 
+		   "term planet = %d, infected stops = %d\n", nplanets, mpaths, 
 		    start, terminal, infectstops);
 
-	printf("infected planets: \n");
-	for (i = 0; i < numofinfectedplanets; i++) {
-		printf("%d ", infplanetid[i]);
-	}
-	printf("\n");
-
-	// Print planets with vaccines
-	printf("Planets with vaccine: \n");
-	for (i = 0; i < planetswithvaccine; i++) {
-		printf("%d ", planetswithvaccineids[i]);
-	}
-	printf("\n");
+	print_graph(graph, nplanets);
 
 	free(infplanetid);
 	free(planetswithvaccineids);
