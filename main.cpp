76503decfa1682cbce0e1d6d49cd04c158651770
Mikhail Saveliev <Savelievmv@gmail.com>
2020-11-17 22:30:45 +0100

    Sorting is ready but not debugged

--- main.cpp
+++ main.cpp
@@ -2,127 +2,254 @@
 #include "flib.h"
 #endif //__PROGTEST__
 
-struct merge_info {
+struct m_info {
     int* arr1;
     int* arr2;
     int* out;
     int arr_size;
 };
 
-// Pseudocode merge function
 // Input parameters:
 //      in1
 //      in2
 //      serie_size
 //      struct merge_info*
 //      out
-void merge(int in1, int in2, int serie_size, struct m_info* m_info)
+// return value:
+//      2 if nothing was read from both files 
+//      (indicates that sorting is over)
+//      1 if nothing was merged
+//      0 otherwise
+int merge(int in1, int in2, int serie_size, struct m_info* m_info, int out)
 {    
     int index1 = m_info->arr_size;
     int index2 = m_info->arr_size;
     int index_out = 0;
-    int rd1 = index1;
-    int rd2 = index2;
-    int total_rd1 = 0;
-    int total_rd2 = 0;
-    int to_read;
+    int64_t rd1 = 0;
+    int64_t rd2 = 0;
+    int64_t total_rd1 = 0;
+    int64_t total_rd2 = 0;
+    int64_t to_read;
+    int64_t read1_zero;
+    int64_t read2_zero;
+    int nf = serie_size / m_info->arr_size;
+    int rem = serie_size % m_info->arr_size;
+    int cnt1 = 0;
+    int cnt2 = 0;
+    int in1_over = 0;
+    int in2_over = 0;
 
-    while (rd1 != 0 || rd2 != 0) {
-        if (index1 == rd1) {
+    while (!in1_over || !in2_over) {
+        if (rd1 == 0) {
             // Read next chunk of serie from file in1
-            to_read = m_info->arr_size; 
-            if (total_rd1 + to_read > serie_size) {
-                to_read = total_rd1 + to_read - serie_size;
+            if (cnt1 == nf) {
+                to_read = rem / sizeof(int);
             }
-            rd1 = read(in1, m_info->arr1, to_read);
+            else {
+                to_read = m_info->arr_size / sizeof(int);
+                cnt1++;
+            }
+            rd1 = flib_read(in1, m_info->arr1, to_read);
+            if (rd1 != to_read || to_read == 0) {
+                in1_over = 1;
+            }
+            read1_zero = rd1;
             index1 = 0;
-            total_rd1 += rd1;
-        }
-        if (index2 == rd2) {
-            // Read next chunk of serie from file in2
-            to_read = m_info->arr_size;
-            if (total_rd2 + to_read > serie_size) {
-                to_read = total_rd2 + to_read - serie_size;
+            total_rd1 += rd1 * sizeof(int);
+            if (total_rd1 == serie_size) {
+                in1_over = 1;
             }
-            rd2 = read(in2, m_info->arr2, to_read);
+            if (total_rd1 > serie_size) {
+                printf("Something is wrong.\n");
+                exit(1);
+            }
+        }
+        if (rd2 == 0) {
+            if (cnt2 == nf) {
+                to_read = rem / sizeof(int);
+            }
+            else {
+                to_read = m_info->arr_size / sizeof(int);
+                cnt2++;
+            }
+            // Read next chunk of serie from file in2
+            rd2 = flib_read(in2, m_info->arr2, to_read);
+            if (rd2 != to_read || to_read == 0) {
+                in2_over = 1;
+            }
+            read2_zero = rd2;
             index2 = 0;
-            total_rd2 += rd2;
+            total_rd2 += rd2 * sizeof(int);
+            if (total_rd2 == serie_size) {
+                in2_over = 1;
+            }
+            if (total_rd2 > serie_size) {
+                printf("Something is wrong.\n");
+                exit(1);
+            }
         }
         
         // Sorting merge of m_info->arr1 and m_info->arr2 to m_info->out
-        for ( ; (index1 < rd1 && index2 < rd2) || (rd1 + rd2 != 0); index_out++) {
+        for ( ; (rd1 > 0 && rd2 > 0) || (rd1 == 0 && rd2 > 0 && in1_over) 
+             || (rd2 == 0 && rd1 > 0 && in2_over); index_out++) {
+            if (index_out == (2 * m_info->arr_size)) {
+                flib_write(out, m_info->out, index_out);
+                index_out = 0;
+            }
             if (rd1 == 0) {
                 // Serie from file in1 is read and merged completely
                 m_info->out[index_out] = m_info->arr2[index2];
                 index2++;
+                rd2--;
                 continue;
             }
             if (rd2 == 0) {
                 // Serie from file in2 is read and merged completely
                 m_info->out[index_out] = m_info->arr1[index1];
                 index1++;
+                rd1--;
                 continue;
             }
             // Merge using two chunks of series
             if (m_info->arr1[index1] > m_info->arr2[index2]) {
                 m_info->out[index_out] = m_info->arr2[index2];
                 index2++;
+                rd2--;
             }
             else {
-                m_info->out[index_out] = m_info->arr2[index1];
+                m_info->out[index_out] = m_info->arr1[index1];
                 index1++;
+                rd1--;
             }
         }
     }
+    flib_write(out, m_info->out, index_out);
+    
+    return (total_rd1 == 0 && total_rd2 == 0);
+}
+
+int cmp(const void* a, const void* b)
+{
+    int a1 = *(const int*)a;
+    int b1 = *(const int*)b;
+
+    if (a1 < b1) {
+        return -1;
+    }
+    return (a1 == b1) ? 0 : 1;
 }
 
 void tarant_allegra(int32_t in_file, int32_t out_file, int32_t bytes) {
 
-    struct merge_info m_info;
+    struct m_info m_info;
     int serie_size;
+    // Auxillary files for merging
+    uint16_t f1 = in_file + out_file + 1;
+    uint16_t f2 = f1 + 1;
+    uint16_t f3 = f2 + 1;
+    uint16_t f4 = f3 + 1;
+    int64_t intloaded;
+    int tmp;
 
     m_info.arr_size = bytes / 4 / sizeof(int) * sizeof(int);
-    m_info.arr1 = malloc(m_info.arr_size);
-    m_info.arr2 = malloc(m_info.arr_size);
-    m_info.out = malloc(m_info.arr_size * 2);
+    m_info.arr1 = (int*)malloc(m_info.arr_size);
+    m_info.arr2 = (int*)malloc(m_info.arr_size);
+    m_info.out = (int*)malloc(m_info.arr_size * 2);
     if (m_info.arr1 == NULL || m_info.arr2 == NULL || m_info.out == NULL) {
         perror("Malloc failed");
         exit(1);
     }
-    // Pseudocode
-    // Open aux file1
-    // Open aux file2
-    // open in_file
-
-while(!eof in_file) {
-    //      read bytes bytes
-    //      sort read data
-    //      write to aux1
-    //      read bytes bytes
-    //      sort read data
-    //      write to aux2
-    // }
-    // close in_file
-    // close aux1
-    // close aux2
-    // 
-    // serie_size = bytes / sizeof(int)
-    serie_size = m_info.arr_size / sizeof(int);
-    while (1) {
     
-    //      open in1
-    //      open in2
-    //      create out1
-    //      create out2
-        merge(in1, in2, serie_size, &m_info);
-        write(out1, m_info.out, ?);
-        merge(in1, in2, serie_size, &m_info);
-        write(out2, m_info.out, ? );
-    //      close in1
-    //      close in2
-    //      rm in1, in2
+    // Initial split
+    flib_open(in_file, READ);
+    flib_open(f1, WRITE);
+    flib_open(f2, WRITE);
+
+    serie_size = m_info.arr_size / sizeof(int);
+
+    while(1) {
+        intloaded = flib_read(in_file, m_info.arr1, serie_size);
+        if (intloaded == 0) {
+            break;
+        }
+        qsort(m_info.arr1, (size_t)intloaded, sizeof(int), cmp);
+        flib_write(f1, m_info.arr1, intloaded);
+        
+        intloaded = flib_read(in_file, m_info.arr1, serie_size);
+        if (intloaded == 0) {
+            break;
+        }
+        qsort(m_info.arr1, (size_t)intloaded, sizeof(int), cmp);
+        flib_write(f2, m_info.arr1, intloaded);
+    }
+    flib_close(in_file);
+    flib_close(f1);
+    flib_close(f2);
+    
+    while (1) {
+        int rc;
+        int merges_nr;
+        flib_open(f1, READ);
+        flib_open(f2, READ);
+        flib_open(f3, WRITE);
+        flib_open(f4, WRITE);
+
+        merges_nr = 0;
+
+        while (1) {
+            rc = merge(f1, f2, serie_size * sizeof(int), &m_info, f3);
+            if (rc == 0) {
+                merges_nr++;
+            } else {
+                break;
+            }
+            rc = merge(f1, f2, serie_size * sizeof(int), &m_info, f4);
+            if (rc == 0) {
+                merges_nr++;
+            } else {
+                break;
+            }
+        }
+        
+        flib_close(f1);
+        flib_close(f2);
+        flib_close(f3);
+        flib_close(f4);
+
+        if (merges_nr == 1) {
+            printf("Sorting is over\n");
+            break;
+        }
+        if (merges_nr == 0) {
+            printf("Something is wrong\n");
+            exit(1);
+        }
+
+        tmp = f1;
+        f1 = f3;
+        f3 = tmp;
+
+        tmp = f2;
+        f2 = f4;
+        f4 = tmp;
+
         serie_size *= 2;
     }
+
+    flib_open(out_file, WRITE);
+    flib_open(f3, READ);
+
+    while (1) {
+        intloaded = flib_read(f3, m_info.out, m_info.arr_size * 2 / sizeof(int));
+        if (intloaded == 0) {
+            break;
+        }
+        flib_write(out_file, m_info.out, intloaded);
+    }
+
+    flib_close(out_file);
+    flib_close(f3);
+
     free(m_info.arr1);
     free(m_info.arr2);
     free(m_info.out);
@@ -180,9 +307,10 @@ int main(int argc, char **argv){
     int INPUT = 0;
     int RESULT = 1;
     int SIZE = 140;
+    SIZE = 40;
 
     create_random(INPUT, SIZE);
-    tarant_allegra(INPUT, RESULT, 1000);
+    tarant_allegra(INPUT, RESULT, 64);
     check_result(RESULT, SIZE);
 
     flib_free_files();
