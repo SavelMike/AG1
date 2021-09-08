a91e80bade9d2a2056093e294504dc6f566daa85
Saveliev Mike <Savelievmv@gmail.com>
2021-09-09 01:27:14 +0200

    Add files via upload
    Graph: adjacency list presentation

---
+++ TestGraph.java
@@ -0,0 +1,12 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestGraph {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.readGraph();
+        graph.printGraph();
+    }
+}
