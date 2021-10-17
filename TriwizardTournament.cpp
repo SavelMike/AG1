2056d3f9863a006868eef3728999c1e022bf1867
Mikhail Saveliev <Savelievmv@gmail.com>
2021-10-17 03:24:53 +0200

    queue is implemented, bfs algorithm implemented, checking adjacents elements added

--- TriwizardTournament.cpp
+++ TriwizardTournament.cpp
@@ -1,7 +1,56 @@
 // TriwizardTournament.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+
+struct coord
+{
+    int x;
+    int y;
+};
+
+struct queue_element 
+{
+    coord cell;
+    struct queue_element* next;
+};
+
+struct 
+{
+    queue_element* head;
+    queue_element* tail;
+} queue = {NULL, NULL};
+
+coord dequeue() {
+    if (queue.tail == NULL) {
+        std::cerr << "Empty queue\n";
+        exit(1);
+    }
+    coord rc = queue.head->cell;
+    queue_element* phead = queue.head;
+    queue.head = queue.head->next;
+    
+    if (queue.head == NULL) {
+        // Queue is empty
+        queue.tail = NULL;
+    }
+    delete phead;
+    
+    return rc;
+}
+
+void enqueue(coord* cell) {
+    queue_element* qe = new struct queue_element;
+    qe->cell = *cell;
+    qe->next = NULL;
+    // Add as last element to queue
+    if (queue.tail == NULL) {
+        queue.head = qe;
+        queue.tail = qe;
+    } else {
+        queue.tail->next = qe;
+    }
+    
+}
 
 // Array of levers will be created while processing the input
 struct Lever
@@ -12,14 +61,9 @@ struct Lever
 
 enum cell_state 
 {
-    undef, opened, closed
+    unmarked, opened, closed
 };
 
-struct coord
-{
-    int x;
-    int y;
-};
 
 struct maze_cell
 {
@@ -31,6 +75,9 @@ struct maze_cell
 
 // Labyrinth
 struct maze_cell** maze;
+
+// Coordinates of goblet
+coord goblet;
 
 void print_levers(struct Lever* levers, int n, int k) {
     for (int i = 0; i < k; i++) {
@@ -58,7 +105,9 @@ void print_maze(int n, coord* goblet) {
     }
 }
 
-int main()
+void bfs(coord* start, int n);
+
+int main(void)
 {
     int n; // Size of labyrinth of one side
     int k; // Num of levers
@@ -107,8 +156,6 @@ int main()
     }
 
     // Read Goblet coordinates
-    coord goblet;
-    
     std::cin >> goblet.x >> goblet.y;
  
     goblet.x--;
@@ -119,6 +166,10 @@ int main()
     print_levers(levers, n, k);
 
     print_maze(n, &goblet);
+
+    coord start = { 0, 0 };
+
+    bfs(&start, n);
     
     // Free memory
     delete[] levers;
@@ -131,10 +182,96 @@ int main()
     return 0;
 }
 
-
-/*
-dfs() {
-    ;
+// Return value: 
+//      true if goblet is found
+//      false otherwise
+bool check_pair(coord* centr, coord* neighbour) {
+    maze_cell* p = &maze[neighbour->x][neighbour->y];
+    if (p->is_wall != true && p->state == unmarked) {
+        p->state = opened;
+        p->distance = maze[centr->x][centr->y].distance + 1;
+        p->predecessor = *centr;
+        // Check if we reached the target
+        if (neighbour->x == goblet.x && neighbour->y == goblet.y) {
+            // Reached the target
+            return true;
+        }
+        enqueue(neighbour);
+    }
+    return false;
 }
-*/
+
+bool check_adjacents(coord* centr, int n) {
+
+    coord neighbour;
+    // Check top
+    if (centr->y != 0) {
+        neighbour = *centr;
+        neighbour.y--;
+        if (check_pair(centr, &neighbour)) {
+            return true;
+        }
+    }
+    // Check right
+    if (centr->x != n - 1) {
+        neighbour = *centr;
+        neighbour.x++;
+        if (check_pair(centr, &neighbour)) {
+            return true;
+        }
+    }
+
+    // Check left
+    if (centr->x != 0) {
+        neighbour = *centr;
+        neighbour.x--;
+        if (check_pair(centr, &neighbour)) {
+            return true;
+        }
+    }
+
+    // Check bottom
+    if (centr->y != n - 1) {
+        neighbour = *centr;
+        neighbour.y++;
+        if (check_pair(centr, &neighbour)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+void bfs(coord* start, int n) {
+    bool found = false;
+    // Initialization of state, distance and predecessor
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            maze[i][j].state = unmarked;
+            maze[i][j].distance = -1;
+            maze[i][j].predecessor.x = -1;
+            maze[i][j].predecessor.y = -1;
+        }    
+    }
+    enqueue(start);
+    maze[start->x][start->y].state = opened;
+    maze[start->x][start->y].distance = 0;
+    while (queue.head != NULL) {
+        coord first_el = dequeue();
+        if (check_adjacents(&first_el, n)) {
+            found = true;
+            break;
+        }
+        maze[first_el.x][first_el.y].state = closed;
+    }
+
+    if (found) {
+        std::cout << "found; path length = " << maze[goblet.x][goblet.y].distance << "\n";
+    }
+    else {
+        std::cout << "Goblet not found\n";
+    }
+}
+
 
