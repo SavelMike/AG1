d79c3db461cad3438d5ee327178a483c4ab8798d
Mikhail Saveliev <Savelievmv@gmail.com>
2021-12-14 01:23:35 +0100

    Maximal number of passengers to be checked and pass sections where inspectors are to stay are calculated within recursive function combinations

--- Transportation/Transportation.cpp
+++ Transportation/Transportation.cpp
@@ -9,14 +9,40 @@ struct pass_record {
     int from;
     int to;
     long long count;
-};
+}; 
 
+// Max number of passengers which can be checked
+long long max_num = 0;
+// Array of path segments where inspectors should be at 
+int* stops_to_check;
+// Array of input passenger records
+struct pass_record* record;
+int nr_records;
+
+long long calculate_passengers(int* segments, int k) {
+    long long passcnt = 0;
+    for (int i = 0; i < nr_records; i++) {
+        for (int j = 0; j < k; j++) {
+            if (segments[j] >= record[i].from && segments[j] < record[i].to) {
+                passcnt += record[i].count;
+                break;
+            }
+        }
+    }
+
+    return passcnt;
+}
+
+// Combinatorics combination
 void combination(int *arrin, int n, int *arrout, int ind, int k) {
     if (ind == k) {
-        for (int i = 0; i < ind; i++) {
-            cout << arrout[i] << ",";
+        long long num = calculate_passengers(arrout, k);
+        if (num > max_num) {
+            max_num = num;
+            for (int i = 0; i < ind; i++) {               
+                stops_to_check[i] = arrout[i];
+            }
         }
-        cout << "\n";
         return;
     }
     for (int i = 0; i < n; i++) {
@@ -29,9 +55,8 @@ int main()
 {
     int c; // number of inspectors
     int s; // number of stops
-    int p; // number of passenger records
     
-    cin >> c >> s >> p;
+    cin >> c >> s >> nr_records;
     // Elements of this array become true if segments passed passengers. 
     bool *active_segments = new bool[s - 1];
     for (int i = 0; i < s - 1; i++) {
@@ -39,8 +64,8 @@ int main()
     }
 
     // Read passenger records
-    struct pass_record *record = new struct pass_record[p];
-    for (int i = 0; i < p; i++) {
+    record = new struct pass_record[nr_records];
+    for (int i = 0; i < nr_records; i++) {
         cin >> record[i].from >> record[i].to >> record[i].count;
         for (int j = record[i].from; j < record[i].to; j++) {
             active_segments[j] = true;
@@ -65,7 +90,22 @@ int main()
         }
     }
     int* comb = new int[c];
+    stops_to_check = new int[c];
     combination(act_segments, cnt, comb, 0, c);
+
+    cout << max_num << "\n";
+    for (int i = 0; i < c; i++) {
+        cout << "[" << stops_to_check[i] << ";" << stops_to_check[i] + 1 << "]";
+        if (i != c - 1) {
+            cout << ",";
+        }
+    }
+    cout << "\n";
+    delete[] active_segments;
+    delete[] act_segments;
+    delete[] stops_to_check;
+    delete[] comb;
+    delete[] record;
 
     return 0;
 }
